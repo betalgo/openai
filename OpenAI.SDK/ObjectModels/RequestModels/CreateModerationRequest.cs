be36c08b68fbd310776460a79485a7e1fc22e964
Tolga Kayhan <kayhantolga@users.noreply.github.com>
2022-12-26 20:59:03 +0000

    Code Cleanup and documentation update

--- OpenAI.SDK/ObjectModels/RequestModels/CreateModerationRequest.cs
+++ OpenAI.SDK/ObjectModels/RequestModels/CreateModerationRequest.cs
@@ -7,15 +7,6 @@ namespace OpenAI.GPT3.ObjectModels.RequestModels
     public record CreateModerationRequest : IOpenAiModels.IModel
     {
         /// <summary>
-        ///     Two content moderations models are available: text-moderation-stable and text-moderation-latest.
-        ///     The default is text-moderation-latest which will be automatically upgraded over time. This ensures you are always
-        ///     using our most accurate model. If you use text-moderation-stable, we will provide advanced notice before updating
-        ///     the model. Accuracy of text-moderation-stable may be slightly lower than for text-moderation-latest.
-        /// </summary>
-        [JsonPropertyName("model")]
-        public string? Model { get; set; }
-        
-        /// <summary>
         ///     The input text to classify
         /// </summary>
         [JsonIgnore]
@@ -46,5 +37,14 @@ namespace OpenAI.GPT3.ObjectModels.RequestModels
                 return InputAsList;
             }
         }
+
+        /// <summary>
+        ///     Two content moderations models are available: text-moderation-stable and text-moderation-latest.
+        ///     The default is text-moderation-latest which will be automatically upgraded over time. This ensures you are always
+        ///     using our most accurate model. If you use text-moderation-stable, we will provide advanced notice before updating
+        ///     the model. Accuracy of text-moderation-stable may be slightly lower than for text-moderation-latest.
+        /// </summary>
+        [JsonPropertyName("model")]
+        public string? Model { get; set; }
     }
 }
