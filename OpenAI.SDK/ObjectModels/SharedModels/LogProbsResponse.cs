d4c8a828dc3b63f6e21c560c6b1669b58289f393
Tolga Kayhan <kayhantolga@users.noreply.github.com>
2023-01-04 20:47:21 +0000

    Arrays converted to List, added developer friendly option for TopLogProbs

--- OpenAI.SDK/ObjectModels/SharedModels/LogProbsResponse.cs
+++ OpenAI.SDK/ObjectModels/SharedModels/LogProbsResponse.cs
@@ -4,11 +4,13 @@ namespace OpenAI.GPT3.ObjectModels.SharedModels;
 
 public record LogProbsResponse
 {
-    [JsonPropertyName("tokens")] public string[] Tokens { get; set; }
+    [JsonPropertyName("tokens")] public List<string> Tokens { get; set; }
 
-    [JsonPropertyName("token_logprobs")] public double[] TokenLogProbs { get; set; }
+    [JsonPropertyName("token_logprobs")] public List<double> TokenLogProbs { get; set; }
 
-    [JsonPropertyName("top_logprobs")] public Dictionary<string,double>[] TopLogProbs { get; set; }
+    [JsonPropertyName("top_logprobs")] public List<Dictionary<string, double>> TopLogProbsRaw { get; set; }
 
-    [JsonPropertyName("text_offset")] public int[] TextOffset { get; set; }
+    public Dictionary<string, double> TopLogProbs => TopLogProbsRaw.SelectMany(r => r).ToDictionary(k => k.Key, k => k.Value);
+
+    [JsonPropertyName("text_offset")] public List<int> TextOffset { get; set; }
 }
