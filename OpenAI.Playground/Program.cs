286cafeda14aafc727372c20f2204e83d0e9d85f
mike@gotmike.com <mike@gotmike.com>
2023-01-27 05:40:19 -0500

    updated to take command-line arguments for prompt and image count. also added values to launch settings;

--- OpenAI.Playground/Program.cs
+++ OpenAI.Playground/Program.cs
@@ -5,6 +5,8 @@ using OpenAI.GPT3.Extensions;
 using OpenAI.GPT3.Interfaces;
 using OpenAI.Playground.TestHelpers;
 
+var commandLineArgs = Environment.GetCommandLineArgs();
+
 var builder = new ConfigurationBuilder()
     .AddJsonFile("ApiSettings.json")
     .AddUserSecrets<Program>();
@@ -24,6 +26,20 @@ serviceCollection.AddOpenAIService();
 var serviceProvider = serviceCollection.BuildServiceProvider();
 var sdk = serviceProvider.GetRequiredService<IOpenAIService>();
 
+var prompt = string.Empty;
+var imageCount = 1;
+foreach (var commandLineArg in commandLineArgs)
+{
+    if (commandLineArg.StartsWith("--prompt:"))
+    {
+        prompt = commandLineArg.Replace("--prompt:","").Replace("\"","");
+    }
+    if (commandLineArg.StartsWith("--imagecount:"))
+    {
+        int.TryParse(commandLineArg.Replace("--imagecount:", ""),out imageCount);
+    }
+}
+
 //await ModelTestHelper.FetchModelsTest(sdk);
 //await EditTestHelper.RunSimpleEditCreateTest(sdk);
 //await ImageTestHelper.RunSimpleCreateImageTest(sdk);
@@ -39,7 +55,7 @@ var sdk = serviceProvider.GetRequiredService<IOpenAIService>();
 //////await FineTuningTestHelper.CleanUpAllFineTunings(sdk); //!!!!! will delete all fine-tunings
 //await FineTuningTestHelper.RunCaseStudyIsTheModelMakingUntrueStatements(sdk);
 
-await ComboTestHelper.RunSimpleComboTest(sdk, "Once upon a time");
+await ComboTestHelper.RunSimpleComboTest(sdk, prompt, imageCount);
 
 Console.WriteLine("Press ENTER to exit.");
 
