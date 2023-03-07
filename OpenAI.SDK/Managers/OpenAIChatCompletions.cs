cb6dd0641bb7195b01091073ec35a83b449535d5
Patrick D'Cruze <Patrick.DCruze@neorise.com.au>
2023-03-07 10:38:49 +0800

    Revert CreateCompletionAsStream to previous implementation so that we don't break .Net 6 async stream behaviour.

--- OpenAI.SDK/Managers/OpenAIChatCompletions.cs
+++ OpenAI.SDK/Managers/OpenAIChatCompletions.cs
@@ -27,64 +27,46 @@ public partial class OpenAIService : IChatCompletionService
         chatCompletionCreateRequest.ProcessModelId(modelId, _defaultModelId);
 
         using var response = _httpClient.PostAsStreamAsync(_endpointProvider.ChatCompletionCreate(), chatCompletionCreateRequest, cancellationToken);
-        Stream? stream = null;
-        try
+        await using var stream = await response.Content.ReadAsStreamAsync(cancellationToken);
+        using var reader = new StreamReader(stream);
+        // Continuously read the stream until the end of it
+        while (!reader.EndOfStream)
         {
-            stream = await response.Content.ReadAsStreamAsync(cancellationToken);
-            using var reader = new StreamReader(stream);
-            // Continuously read the stream until the end of it
-            while (!reader.EndOfStream)
+            cancellationToken.ThrowIfCancellationRequested();
+
+            var line = await reader.ReadLineAsync();
+            // Skip empty lines
+            if (string.IsNullOrEmpty(line))
             {
-                cancellationToken.ThrowIfCancellationRequested();
-
-                var line = await reader.ReadLineAsync();
-                // Skip empty lines
-                if (string.IsNullOrEmpty(line))
-                {
-                    continue;
-                }
-
-                line = line.RemoveIfStartWith("data: ");
-
-                // Exit the loop if the stream is done
-                if (line.StartsWith("[DONE]"))
-                {
-                    break;
-                }
-
-                ChatCompletionCreateResponse? block;
-                try
-                {
-                    // When the response is good, each line is a serializable CompletionCreateRequest
-                    block = JsonSerializer.Deserialize<ChatCompletionCreateResponse>(line);
-                }
-                catch (Exception)
-                {
-                    // When the API returns an error, it does not come back as a block, it returns a single character of text ("{").
-                    // In this instance, read through the rest of the response, which should be a complete object to parse.
-                    line += await reader.ReadToEndAsync();
-                    block = JsonSerializer.Deserialize<ChatCompletionCreateResponse>(line);
-                }
-
-
-                if (null != block)
-                {
-                    yield return block;
-                }
+                continue;
             }
-        }
-        finally
-        {
-            if (stream is not null)
+
+            line = line.RemoveIfStartWith("data: ");
+
+            // Exit the loop if the stream is done
+            if (line.StartsWith("[DONE]"))
             {
-                if (stream is IAsyncDisposable asyncDisposable)
-                {
-                    await asyncDisposable.DisposeAsync();
-                }
-                else
-                {
-                    stream.Dispose();
-                }
+                break;
+            }
+
+            ChatCompletionCreateResponse? block;
+            try
+            {
+                // When the response is good, each line is a serializable CompletionCreateRequest
+                block = JsonSerializer.Deserialize<ChatCompletionCreateResponse>(line);
+            }
+            catch (Exception)
+            {
+                // When the API returns an error, it does not come back as a block, it returns a single character of text ("{").
+                // In this instance, read through the rest of the response, which should be a complete object to parse.
+                line += await reader.ReadToEndAsync();
+                block = JsonSerializer.Deserialize<ChatCompletionCreateResponse>(line);
+            }
+
+
+            if (null != block)
+            {
+                yield return block;
             }
         }
     }
--- OpenAI.SDK/Managers/OpenAICompletions.cs
+++ OpenAI.SDK/Managers/OpenAICompletions.cs
@@ -26,64 +26,46 @@ public partial class OpenAIService : ICompletionService
         createCompletionRequest.ProcessModelId(modelId, _defaultModelId);
 
         using var response = _httpClient.PostAsStreamAsync(_endpointProvider.CompletionCreate(), createCompletionRequest, cancellationToken);
-        Stream? stream = null;
-        try
+        await using var stream = await response.Content.ReadAsStreamAsync(cancellationToken);
+        using var reader = new StreamReader(stream);
+        // Continuously read the stream until the end of it
+        while (!reader.EndOfStream)
         {
-            stream = await response.Content.ReadAsStreamAsync(cancellationToken);
-            using var reader = new StreamReader(stream);
-            // Continuously read the stream until the end of it
-            while (!reader.EndOfStream)
+            cancellationToken.ThrowIfCancellationRequested();
+
+            var line = await reader.ReadLineAsync();
+            // Skip empty lines
+            if (string.IsNullOrEmpty(line))
             {
-                cancellationToken.ThrowIfCancellationRequested();
-
-                var line = await reader.ReadLineAsync();
-                // Skip empty lines
-                if (string.IsNullOrEmpty(line))
-                {
-                    continue;
-                }
-
-                line = line.RemoveIfStartWith("data: ");
-
-                // Exit the loop if the stream is done
-                if (line.StartsWith("[DONE]"))
-                {
-                    break;
-                }
-
-                CompletionCreateResponse? block;
-                try
-                {
-                    // When the response is good, each line is a serializable CompletionCreateRequest
-                    block = JsonSerializer.Deserialize<CompletionCreateResponse>(line);
-                }
-                catch (Exception)
-                {
-                    // When the API returns an error, it does not come back as a block, it returns a single character of text ("{").
-                    // In this instance, read through the rest of the response, which should be a complete object to parse.
-                    line += await reader.ReadToEndAsync();
-                    block = JsonSerializer.Deserialize<CompletionCreateResponse>(line);
-                }
-
-
-                if (null != block)
-                {
-                    yield return block;
-                }
+                continue;
             }
-        }
-        finally
-        {
-            if (stream is not null)
+
+            line = line.RemoveIfStartWith("data: ");
+
+            // Exit the loop if the stream is done
+            if (line.StartsWith("[DONE]"))
             {
-                if (stream is IAsyncDisposable asyncDisposable)
-                {
-                    await asyncDisposable.DisposeAsync();
-                }
-                else
-                {
-                    stream.Dispose();
-                }
+                break;
+            }
+
+            CompletionCreateResponse? block;
+            try
+            {
+                // When the response is good, each line is a serializable CompletionCreateRequest
+                block = JsonSerializer.Deserialize<CompletionCreateResponse>(line);
+            }
+            catch (Exception)
+            {
+                // When the API returns an error, it does not come back as a block, it returns a single character of text ("{").
+                // In this instance, read through the rest of the response, which should be a complete object to parse.
+                line += await reader.ReadToEndAsync();
+                block = JsonSerializer.Deserialize<CompletionCreateResponse>(line);
+            }
+
+
+            if (null != block)
+            {
+                yield return block;
             }
         }
     }
