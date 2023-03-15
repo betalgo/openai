6dcfdc6d1cd4ae69e55e9269c7707c6a56f36baa
Shane Powell <killerbee@club88.co.nz>
2023-03-15 14:11:54 +1300

    Add support for the optional "name" into the ChatMessage class.
    See https://github.com/openai/openai-cookbook/blob/main/examples/How_to_format_inputs_to_ChatGPT_models.ipynb
    in the "Few-shot prompting" section and "Counting tokens" section for
    examples of use.

--- OpenAI.SDK/ObjectModels/RequestModels/ChatMessage.cs
+++ OpenAI.SDK/ObjectModels/RequestModels/ChatMessage.cs
@@ -4,14 +4,15 @@ namespace OpenAI.GPT3.ObjectModels.RequestModels;
 
 /// <summary>
 ///     Messages must be an array of message objects, where each object has a role (either “system”, “user”, or
-///     “assistant”) and content (the content of the message)
+///     “assistant”) and content (the content of the message) and an optional name
 /// </summary>
 public class ChatMessage
 {
-    public ChatMessage(string role, string content)
+    public ChatMessage(string role, string content, string? name = null)
     {
         Role = role;
         Content = content;
+        Name = name;
     }
 
     /// <summary>
@@ -26,9 +27,20 @@ public class ChatMessage
     [JsonPropertyName("content")]
     public string Content { get; set; }
 
+    /// <summary>
+    ///     Optional Message user Name
+    /// </summary>
+    [JsonPropertyName("name")]
+    public string? Name { get; set; }
+
     public static ChatMessage FromAssistant(string content)
     {
         return new ChatMessage(StaticValues.ChatMessageRoles.Assistant, content);
+    }
+
+    public static ChatMessage FromAssistant(string name, string content)
+    {
+        return new ChatMessage(StaticValues.ChatMessageRoles.Assistant, content, name);
     }
 
     public static ChatMessage FromUser(string content)
@@ -36,8 +48,17 @@ public class ChatMessage
         return new ChatMessage(StaticValues.ChatMessageRoles.User, content);
     }
 
+    public static ChatMessage FromUser(string name, string content)
+    {
+        return new ChatMessage(StaticValues.ChatMessageRoles.User, content, name);
+    }
+
     public static ChatMessage FromSystem(string content)
     {
         return new ChatMessage(StaticValues.ChatMessageRoles.System, content);
+    }
+    public static ChatMessage FromSystem(string name, string content)
+    {
+        return new ChatMessage(StaticValues.ChatMessageRoles.System, content, name);
     }
 }
