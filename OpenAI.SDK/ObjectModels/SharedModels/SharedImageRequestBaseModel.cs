3468323740161f88d52c14eaeb56b63db1f6e30f
Tolga Kayhan <kayhantolga@users.noreply.github.com>
2023-12-03 10:44:42 +0000

    Added StaticValues for ImageStatics.Quality

--- OpenAI.SDK/ObjectModels/SharedModels/SharedImageRequestBaseModel.cs
+++ OpenAI.SDK/ObjectModels/SharedModels/SharedImageRequestBaseModel.cs
@@ -15,6 +15,7 @@ public record SharedImageRequestBaseModel
     ///     The size of the generated images. 
     ///     Must be one of 256x256, 512x512, or 1024x1024 for dall-e-2. 
     ///     Must be one of 1024x1024, 1792x1024, or 1024x1792 for dall-e-3 models.
+    ///     <br /><br />Check <see cref="StaticValues.ImageStatics.Size"/> for possible values
     /// </summary>
     [JsonPropertyName("size")]
     public string? Size { get; set; }
