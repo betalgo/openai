947a55cdb298a1f8405bb009bff7c41bb3401cee
Tolga Kayhan <kayhantolga@users.noreply.github.com>
2024-04-11 18:13:21 +0100

    console color reverted to default

--- OpenAI.Playground/ConsoleExtensions.cs
+++ OpenAI.Playground/ConsoleExtensions.cs
@@ -2,7 +2,7 @@
 
 public static class ConsoleExtensions
 {
-    public static void WriteLine(string value, ConsoleColor color = ConsoleColor.Cyan)
+    public static void WriteLine(string value, ConsoleColor color = ConsoleColor.Gray)
     {
         var defaultColor = Console.ForegroundColor;
         Console.ForegroundColor = color;
