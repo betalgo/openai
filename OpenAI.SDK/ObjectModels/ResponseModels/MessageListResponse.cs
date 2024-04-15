88f4be9f1ef4de0a7f34d39cc6c36f0b6db13d6e
Tolga Kayhan <kayhantolga@users.noreply.github.com>
2024-04-15 22:24:48 +0100

    code cleanup

--- OpenAI.SDK/ObjectModels/ResponseModels/MessageListResponse.cs
+++ OpenAI.SDK/ObjectModels/ResponseModels/MessageListResponse.cs
@@ -1,22 +1,16 @@
-﻿using OpenAI.ObjectModels.SharedModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
+﻿using System.Text.Json.Serialization;
+using OpenAI.ObjectModels.SharedModels;
 
-namespace OpenAI.ObjectModels.ResponseModels
+namespace OpenAI.ObjectModels.ResponseModels;
+
+public record MessageListResponse : DataBaseResponse<List<MessageResponse>>
 {
-    public record MessageListResponse : DataBaseResponse<List<MessageResponse>>
-    {
-        [JsonPropertyName("first_id")]
-        public string FirstId {  get; set; }
+    [JsonPropertyName("first_id")]
+    public string FirstId { get; set; }
 
-        [JsonPropertyName("last_id")]
-        public string LastId { get; set; }
+    [JsonPropertyName("last_id")]
+    public string LastId { get; set; }
 
-        [JsonPropertyName("has_more")]
-        public bool IsHasMore { get; set; }
-    }
+    [JsonPropertyName("has_more")]
+    public bool IsHasMore { get; set; }
 }
