8cbb340387dd78ced16c463280fc09bb2d17a6f7
Tolga Kayhan <kayhantolga@users.noreply.github.com>
2024-04-15 21:54:07 +0100

    documentation typo fixes

--- OpenAI.SDK/ObjectModels/RequestModels/ThreadCreateRequest.cs
+++ OpenAI.SDK/ObjectModels/RequestModels/ThreadCreateRequest.cs
@@ -12,9 +12,9 @@ public class ThreadCreateRequest : IOpenAiModels.IMetaData
     public List<MessageCreateRequest>? Messages { get; set; }
 
     /// <summary>
-    ///     Set of 16 key-value pairs that can be attached to an object. This can be useful for storing additional information
-    ///     about the object in a structured format. Keys can be a maximum of 64 characters long and values can be a maxium of
-    ///     512 characters lon
+    ///     Set of 16 key-value pairs that can be attached to an object.
+    ///     This can be useful for storing additional information about the object in a structured format.
+    ///     Keys can be a maximum of 64 characters long and values can be a maximum of 512 characters long.
     /// </summary>
     [JsonPropertyName("metadata")]
     public Dictionary<string, string>? MetaData { get; set; }
--- OpenAI.SDK/ObjectModels/SharedModels/ThreadResponse.cs
+++ OpenAI.SDK/ObjectModels/SharedModels/ThreadResponse.cs
@@ -19,6 +19,8 @@ public record ThreadResponse : BaseResponse, IOpenAiModels.IId, IOpenAiModels.IC
 
     /// <summary>
     ///     Set of 16 key-value pairs that can be attached to an object.
+    ///     This can be useful for storing additional information about the object in a structured format.
+    ///     Keys can be a maximum of 64 characters long and values can be a maximum of 512 characters long.
     /// </summary>
     [JsonPropertyName("metadata")]
     public Dictionary<string, string>? MetaData { get; set; }
