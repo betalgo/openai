1fe97035fd62b8a57bd37436f5114a1a88e22f29
Tolga Kayhan <kayhantolga@users.noreply.github.com>
2024-06-03 13:12:50 +0100

    The routes for Azure OpenAI Assistant are fixed.

--- OpenAI.SDK/EndpointProviders/AzureOpenAiEndpointProvider.cs
+++ OpenAI.SDK/EndpointProviders/AzureOpenAiEndpointProvider.cs
@@ -19,7 +19,7 @@ internal class AzureOpenAiEndpointProvider : IOpenAiEndpointProvider
 
     private string Prefix => $"{ApiPrefix}/{DeploymentsPrefix}/{WebUtility.UrlEncode(_deploymentId)}";
     private string AzureVersionQueryString => $"?api-version={_apiVersion}";
-
+    private string AssistantPrefix => $"{ApiPrefix}/";
     public string ModelRetrieve(string model)
     {
         return $"{Prefix}/models/{model}{AzureVersionQueryString}";
@@ -185,27 +185,27 @@ internal class AzureOpenAiEndpointProvider : IOpenAiEndpointProvider
 
     public string AssistantCreate()
     {
-        return $"{Prefix}/assistants{AzureVersionQueryString}";
+        return $"{AssistantPrefix}/assistants{AzureVersionQueryString}";
     }
 
     public string AssistantRetrieve(string assistantId)
     {
-        return $"{Prefix}/assistants/{assistantId}{AzureVersionQueryString}";
+        return $"{AssistantPrefix}/assistants/{assistantId}{AzureVersionQueryString}";
     }
 
     public string AssistantModify(string assistantId)
     {
-        return $"{Prefix}/assistants/{assistantId}{AzureVersionQueryString}";
+        return $"{AssistantPrefix}/assistants/{assistantId}{AzureVersionQueryString}";
     }
 
     public string AssistantDelete(string assistantId)
     {
-        return $"{Prefix}/assistants/{assistantId}{AzureVersionQueryString}";
+        return $"{AssistantPrefix}/assistants/{assistantId}{AzureVersionQueryString}";
     }
 
     public string AssistantList(PaginationRequest? assistantListRequest)
     {
-        var url = $"{Prefix}/assistants{AzureVersionQueryString}";
+        var url = $"{AssistantPrefix}/assistants{AzureVersionQueryString}";
 
         var query = assistantListRequest?.GetQueryParameters();
         if (!string.IsNullOrWhiteSpace(query))
@@ -218,22 +218,22 @@ internal class AzureOpenAiEndpointProvider : IOpenAiEndpointProvider
 
     public string AssistantFileCreate(string assistantId)
     {
-        return $"{Prefix}/assistants/{assistantId}/files{AzureVersionQueryString}";
+        return $"{AssistantPrefix}/assistants/{assistantId}/files{AzureVersionQueryString}";
     }
 
     public string AssistantFileRetrieve(string assistantId, string fileId)
     {
-        return $"{Prefix}/assistants/{assistantId}/files/{fileId}{AzureVersionQueryString}";
+        return $"{AssistantPrefix}/assistants/{assistantId}/files/{fileId}{AzureVersionQueryString}";
     }
 
     public string AssistantFileDelete(string assistantId, string fileId)
     {
-        return $"{Prefix}/assistants/{assistantId}/files/{fileId}{AzureVersionQueryString}";
+        return $"{AssistantPrefix}/assistants/{assistantId}/files/{fileId}{AzureVersionQueryString}";
     }
 
     public string AssistantFileList(string assistantId, PaginationRequest? assistantFileListRequest)
     {
-        var url = $"{Prefix}/assistants/files{AzureVersionQueryString}";
+        var url = $"{AssistantPrefix}/assistants/files{AzureVersionQueryString}";
 
         var query = assistantFileListRequest?.GetQueryParameters();
         if (!string.IsNullOrWhiteSpace(query))
@@ -246,42 +246,42 @@ internal class AzureOpenAiEndpointProvider : IOpenAiEndpointProvider
 
     public string ThreadCreate()
     {
-        return $"{Prefix}/threads{AzureVersionQueryString}";
+        return $"{AssistantPrefix}/threads{AzureVersionQueryString}";
     }
 
     public string ThreadRetrieve(string threadId)
     {
-        return $"{Prefix}/threads/{threadId}{AzureVersionQueryString}";
+        return $"{AssistantPrefix}/threads/{threadId}{AzureVersionQueryString}";
     }
 
     public string ThreadModify(string threadId)
     {
-        return $"{Prefix}/threads/{threadId}{AzureVersionQueryString}";
+        return $"{AssistantPrefix}/threads/{threadId}{AzureVersionQueryString}";
     }
 
     public string ThreadDelete(string threadId)
     {
-        return $"{Prefix}/threads/{threadId}{AzureVersionQueryString}";
+        return $"{AssistantPrefix}/threads/{threadId}{AzureVersionQueryString}";
     }
 
     public string MessageCreate(string threadId)
     {
-        return $"{Prefix}/threads/{threadId}/messages{AzureVersionQueryString}";
+        return $"{AssistantPrefix}/threads/{threadId}/messages{AzureVersionQueryString}";
     }
 
     public string MessageRetrieve(string threadId, string messageId)
     {
-        return $"{Prefix}/threads/{threadId}/messages/{messageId}{AzureVersionQueryString}";
+        return $"{AssistantPrefix}/threads/{threadId}/messages/{messageId}{AzureVersionQueryString}";
     }
 
     public string MessageModify(string threadId, string messageId)
     {
-        return $"{Prefix}/threads/{threadId}/messages/{messageId}{AzureVersionQueryString}";
+        return $"{AssistantPrefix}/threads/{threadId}/messages/{messageId}{AzureVersionQueryString}";
     }
 
     public string MessageList(string threadId, PaginationRequest? messageListRequest)
     {
-        var url = $"{Prefix}/threads/{threadId}/messages{AzureVersionQueryString}";
+        var url = $"{AssistantPrefix}/threads/{threadId}/messages{AzureVersionQueryString}";
 
         var query = messageListRequest?.GetQueryParameters();
         if (!string.IsNullOrWhiteSpace(query))
@@ -293,27 +293,27 @@ internal class AzureOpenAiEndpointProvider : IOpenAiEndpointProvider
     }
     public string MessageDelete(string threadId, string messageId)
     {
-        return $"{Prefix}/threads/{threadId}/messages/{messageId}{AzureVersionQueryString}";
+        return $"{AssistantPrefix}/threads/{threadId}/messages/{messageId}{AzureVersionQueryString}";
     }
 
     public string RunCreate(string threadId)
     {
-        return $"{Prefix}/threads/{threadId}/runs{AzureVersionQueryString}";
+        return $"{AssistantPrefix}/threads/{threadId}/runs{AzureVersionQueryString}";
     }
 
     public string RunRetrieve(string threadId, string runId)
     {
-        return $"{Prefix}/threads/{threadId}/runs/{runId}{AzureVersionQueryString}";
+        return $"{AssistantPrefix}/threads/{threadId}/runs/{runId}{AzureVersionQueryString}";
     }
 
     public string RunModify(string threadId, string runId)
     {
-        return $"{Prefix}/threads/{threadId}/runs/{runId}{AzureVersionQueryString}";
+        return $"{AssistantPrefix}/threads/{threadId}/runs/{runId}{AzureVersionQueryString}";
     }
 
     public string RunList(string threadId, PaginationRequest? runListRequest)
     {
-        var url = $"{Prefix}/threads/{threadId}/runs{AzureVersionQueryString}";
+        var url = $"{AssistantPrefix}/threads/{threadId}/runs{AzureVersionQueryString}";
 
         var query = runListRequest?.GetQueryParameters();
         if (!string.IsNullOrWhiteSpace(query))
@@ -326,27 +326,27 @@ internal class AzureOpenAiEndpointProvider : IOpenAiEndpointProvider
 
     public string RunSubmitToolOutputs(string threadId, string runId)
     {
-        return $"{Prefix}/threads/{threadId}/runs/{runId}/submit_tool_outputs{AzureVersionQueryString}";
+        return $"{AssistantPrefix}/threads/{threadId}/runs/{runId}/submit_tool_outputs{AzureVersionQueryString}";
     }
 
     public string RunCancel(string threadId, string runId)
     {
-        return $"{Prefix}/threads/{threadId}/runs/{runId}/cancel{AzureVersionQueryString}";
+        return $"{AssistantPrefix}/threads/{threadId}/runs/{runId}/cancel{AzureVersionQueryString}";
     }
 
     public string ThreadAndRunCreate()
     {
-        return $"{Prefix}/threads/runs{AzureVersionQueryString}";
+        return $"{AssistantPrefix}/threads/runs{AzureVersionQueryString}";
     }
 
     public string RunStepRetrieve(string threadId, string runId, string stepId)
     {
-        return $"{Prefix}/threads/{threadId}/runs/{runId}/steps/{stepId}{AzureVersionQueryString}";
+        return $"{AssistantPrefix}/threads/{threadId}/runs/{runId}/steps/{stepId}{AzureVersionQueryString}";
     }
 
     public string RunStepList(string threadId, string runId, PaginationRequest? runStepListRequest)
     {
-        var url = $"{Prefix}/threads/{threadId}/runs/{runId}/steps{AzureVersionQueryString}";
+        var url = $"{AssistantPrefix}/threads/{threadId}/runs/{runId}/steps{AzureVersionQueryString}";
 
         var query = runStepListRequest?.GetQueryParameters();
         if (!string.IsNullOrWhiteSpace(query))
@@ -360,12 +360,12 @@ internal class AzureOpenAiEndpointProvider : IOpenAiEndpointProvider
 
     public string VectorStoreCreate()
     {
-        return $"{Prefix}/vector_stores{AzureVersionQueryString}";
+        return $"{AssistantPrefix}/vector_stores{AzureVersionQueryString}";
     }
 
     public string VectorStoreList(PaginationRequest baseListRequest)
     {
-        var url = $"{Prefix}/vector_stores{AzureVersionQueryString}";
+        var url = $"{AssistantPrefix}/vector_stores{AzureVersionQueryString}";
 
         var query = baseListRequest?.GetQueryParameters();
         if (!string.IsNullOrWhiteSpace(query))
@@ -378,37 +378,37 @@ internal class AzureOpenAiEndpointProvider : IOpenAiEndpointProvider
 
     public string VectorStoreRetrieve(string vectorStoreId)
     {
-        return $"{Prefix}/vector_stores/{vectorStoreId}{AzureVersionQueryString}";
+        return $"{AssistantPrefix}/vector_stores/{vectorStoreId}{AzureVersionQueryString}";
     }
 
     public string VectorStoreModify(string vectorStoreId)
     {
-        return $"{Prefix}/vector_stores/{vectorStoreId}{AzureVersionQueryString}";
+        return $"{AssistantPrefix}/vector_stores/{vectorStoreId}{AzureVersionQueryString}";
     }
 
     public string VectorStoreDelete(string vectorStoreId)
     {
-        return $"{Prefix}/vector_stores/{vectorStoreId}{AzureVersionQueryString}";
+        return $"{AssistantPrefix}/vector_stores/{vectorStoreId}{AzureVersionQueryString}";
     }
 
     public string VectorStoreFileCreate(string vectorStoreId)
     {
-        return $"{Prefix}/vector_stores/{vectorStoreId}/files{AzureVersionQueryString}";
+        return $"{AssistantPrefix}/vector_stores/{vectorStoreId}/files{AzureVersionQueryString}";
     }
 
     public string VectorStoreFileRetrieve(string vectorStoreId, string fileId)
     {
-        return $"{Prefix}/vector_stores/{vectorStoreId}/files/{fileId}{AzureVersionQueryString}";
+        return $"{AssistantPrefix}/vector_stores/{vectorStoreId}/files/{fileId}{AzureVersionQueryString}";
     }
 
     public string VectorStoreFileDelete(string vectorStoreId, string fileId)
     {
-        return $"{Prefix}/vector_stores/{vectorStoreId}/files/{fileId}{AzureVersionQueryString}";
+        return $"{AssistantPrefix}/vector_stores/{vectorStoreId}/files/{fileId}{AzureVersionQueryString}";
     }
 
     public string VectorStoreFileList(string vectorStoreId, VectorStoreFileListRequest? baseListRequest)
     {
-        var url = $"{Prefix}/vector_stores/{vectorStoreId}/files{AzureVersionQueryString}";
+        var url = $"{AssistantPrefix}/vector_stores/{vectorStoreId}/files{AzureVersionQueryString}";
 
         var query = baseListRequest?.GetQueryParameters();
         if (!string.IsNullOrWhiteSpace(query))
@@ -421,22 +421,22 @@ internal class AzureOpenAiEndpointProvider : IOpenAiEndpointProvider
 
     public string VectorStoreFileBatchCreate(string vectorStoreId)
     {
-        return $"{Prefix}/vector_stores/{vectorStoreId}/files/batches{AzureVersionQueryString}";
+        return $"{AssistantPrefix}/vector_stores/{vectorStoreId}/files/batches{AzureVersionQueryString}";
     }
 
     public string VectorStoreFileBatchRetrieve(string vectorStoreId, string batchId)
     {
-        return $"{Prefix}/vector_stores/{vectorStoreId}/files/batches/{batchId}{AzureVersionQueryString}";
+        return $"{AssistantPrefix}/vector_stores/{vectorStoreId}/files/batches/{batchId}{AzureVersionQueryString}";
     }
 
     public string VectorStoreFileBatchCancel(string vectorStoreId, string batchId)
     {
-        return $"{Prefix}/vector_stores/{vectorStoreId}/files/batches/{batchId}/cancel{AzureVersionQueryString}";
+        return $"{AssistantPrefix}/vector_stores/{vectorStoreId}/files/batches/{batchId}/cancel{AzureVersionQueryString}";
     }
 
     public string VectorStoreFileBatchList(string vectorStoreId, string batchId, PaginationRequest? baseListRequest)
     {
-        var url = $"{Prefix}/vector_stores/{vectorStoreId}/files/batches{AzureVersionQueryString}";
+        var url = $"{AssistantPrefix}/vector_stores/{vectorStoreId}/files/batches{AzureVersionQueryString}";
 
         var query = baseListRequest?.GetQueryParameters();
         if (!string.IsNullOrWhiteSpace(query))
