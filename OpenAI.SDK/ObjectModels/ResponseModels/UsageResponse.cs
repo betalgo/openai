e3a8780af66e86420312457ea25b95ebe139c23c
Oleksandr Khytrov <o.khytrov@gmail.com>
2024-09-15 09:55:06 +0300

    Add supprort for reasoning models

--- OpenAI.SDK/ObjectModels/ResponseModels/UsageResponse.cs
+++ OpenAI.SDK/ObjectModels/ResponseModels/UsageResponse.cs
@@ -4,12 +4,15 @@ namespace OpenAI.ObjectModels.ResponseModels;
 
 public record UsageResponse
 {
-    [JsonPropertyName("prompt_tokens")]
+    [JsonPropertyName("prompt_tokens")] 
     public int PromptTokens { get; set; }
 
     [JsonPropertyName("completion_tokens")]
     public int? CompletionTokens { get; set; }
 
-    [JsonPropertyName("total_tokens")]
+    [JsonPropertyName("total_tokens")] 
     public int TotalTokens { get; set; }
+
+    [JsonPropertyName("completion_tokens_details")]
+    public CompletionTokensDetails? CompletionTokensDetails { get; set; }
 }
