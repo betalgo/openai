a03187e3823622b9fdad7ff4040f4bd74739642a
jmuller3 <JMuller3@yahoo.com>
2024-10-28 10:41:37 -0400

    adds missing props to AssistantResponse from #657

--- OpenAI.SDK/ObjectModels/SharedModels/AssistantResponse.cs
+++ OpenAI.SDK/ObjectModels/SharedModels/AssistantResponse.cs
@@ -88,4 +88,20 @@ public record AssistantResponse : BaseResponse, IOpenAIModels.IId, IOpenAIModels
     /// </summary>
     [JsonPropertyName("tools")]
     public List<ToolDefinition> Tools { get; set; }
+
+    /// <summary>
+    ///     What sampling temperature to use, between 0 and 2. Higher values like 0.8 will make the output more random, while
+    ///     lower values like 0.2 will make it more focused and deterministic.
+    /// </summary>
+    [JsonPropertyName("temperature")]
+    public float? Temperature { get; set; }
+
+    /// <summary>
+    ///     An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the
+    ///     tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are
+    ///     considered.
+    ///     We generally recommend altering this or temperature but not both.
+    /// </summary>
+    [JsonPropertyName("top_p")]
+    public double? TopP { get; set; }
 }
